f9430870efb1cd10cadb477e6031893ccb03abe1
Chris Gilmore <chris.gilmore@gmail.com>
2026-04-05 02:11:12 -0500

    Rounds to nearest integer when converting from float to integer

--- src/client/069170.c
+++ src/client/069170.c
@@ -1,5 +1,17 @@
 #include "common.h"
 
+/*
+  cvt.w.s    $ft0, $fa0
+  mfc1       $v0, $ft0
+  jr         $ra
+   nop
+*/
+
+/*
+  cvt.w.s converts float to integer
+  rounding mode on the N64 uses FPCSR_RM_RN (round to nearest)
+*/
+
 s32 func_800A2EF0(f32 arg0) {
-  return arg0;
+  return lroundf(arg0);
 }
